86687e4f95ece5eaa7bea91559d1d0f997d49041
tolga.kurtulus <tolga.kurtulus@odeontechnology.com>
2022-01-27 09:33:08 +0300

    spinner add boostrap

--- src/containers/LoadingContainer.jsx
+++ src/containers/LoadingContainer.jsx
@@ -1,25 +1,10 @@
 import React from "react";
 
 const LoadingContainer = () => {
-  const cSpinnerBorder = {
-    display: "inline-block",
-    width: "2rem",
-    height: "2rem",
-    verticalAlign: "-0.125em",
-    border: "0.25em solid currentColor",
-    borderRightColor: "transparent",
-    borderRadius: "50%",
-    animation: ".75s linear infinite spinner-border",
-    color: "orange",
-  };
-
   return (
     <>
-      {/* <div className="c-spinner-border" role="status">
-        <span className="c-sr-only"></span>
-      </div> */}
-      <div style={cSpinnerBorder} role="status">
-        <span className="sr-only"></span>
+      <div class="spinner-border text-danger" role="status">
+        <span class="sr-only"></span>
       </div>
     </>
   );
